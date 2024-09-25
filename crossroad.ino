082042b510a54fab96e48ff2ea58cfe21dc587ca
user <user@users-MacBook-Pro.local>
2024-09-25 22:41:42 +0300

    Version with named variables

--- crossroad.ino
+++ crossroad.ino
@@ -1,11 +1,20 @@
 #include <NanitLib.h>
 
-byte portSignals[6] = {P3_2, P3_3, P3_4, P4_2, P4_3, P4_4};
+byte firstRedLed = P3_2;
+byte firstYellowLed = P3_3;
+byte firstGreenLed = P3_4;
+
+byte secondRedLed = P4_4;
+byte secondYellowLed = P4_3;
+byte secondGreenLed = P4_2;
+
+byte leds[6] = {firstRedLed, firstYellowLed,
+  firstGreenLed, secondRedLed, secondYellowLed, secondGreenLed};
 void setup() {
   Nanit_Base_Start();
 
-  for(byte portSignal : portSignals) {
-    pinMode(portSignal, OUTPUT);
+  for(byte led : leds) {
+    pinMode(led, OUTPUT);
   }
 }
 
@@ -24,11 +33,11 @@ void loop() {
   delay(1500);
 }
 
-void twoTrafficLight (bool secondSignal, bool thirdSignal, bool fourthSignal) {
-  digitalWrite(P3_2, secondSignal);
-  digitalWrite(P4_2, secondSignal);
-  digitalWrite(P3_3, thirdSignal);
-  digitalWrite(P4_3, thirdSignal);
-  digitalWrite(P3_4, fourthSignal);
-  digitalWrite(P4_4, fourthSignal);
+void twoTrafficLight (bool secondDigitalSignal, bool thirdDigitalSignal, bool fourthDigitalSignal) {
+  digitalWrite(firstRedLed, secondDigitalSignal);
+  digitalWrite(secondGreenLed, secondDigitalSignal);
+  digitalWrite(firstYellowLed, thirdDigitalSignal);
+  digitalWrite(secondYellowLed, thirdDigitalSignal);
+  digitalWrite(firstGreenLed, fourthDigitalSignal);
+  digitalWrite(secondRedLed, fourthDigitalSignal);
 }
